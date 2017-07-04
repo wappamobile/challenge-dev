eb9fbed29f9ac48d568c906269348d1c50599c14
Jeffersonn <jeffersonnlucas@gmail.com>
2017-07-04 00:10:03 -0300

    inclusao do Equals e GetHasCode

--- Wappa.Framework.Model/Abstrato/Pessoa.cs
+++ Wappa.Framework.Model/Abstrato/Pessoa.cs
@@ -1,4 +1,6 @@
-﻿using Wappa.Framework.Model.Comum;
+﻿using System;
+using System.Collections.Generic;
+using Wappa.Framework.Model.Comum;
 
 namespace Wappa.Framework.Model.Abstrato
 {
@@ -18,8 +20,35 @@ namespace Wappa.Framework.Model.Abstrato
         public string Sobrenome { get; set; }
 
         /// <summary>
+        /// Uniao de Nome e Sobrenome
+        /// </summary>
+        public string NomeCompleto { get { return $"{this.Nome} {this.Sobrenome}"; } }
+
+        /// <summary>
         /// Dados de onde reside
         /// </summary>
         public Endereco Endereco { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || this.GetType() != obj.GetType())
+            {
+                return false;
+            }
+            else
+            {
+                Pessoa outro = (Pessoa)obj;
+
+                return this.NomeCompleto.Equals(outro.NomeCompleto);
+            }
+
+        }
+
+        public override int GetHashCode()
+        {
+            int hashNome = this.NomeCompleto.GetHashCode();
+            
+            return (hashNome << 5) + hashNome;
+        }
     }
 }
