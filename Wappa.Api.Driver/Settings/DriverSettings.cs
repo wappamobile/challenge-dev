74a1dcbde3ebd0b178729c5da18b2737ffbedebc
Jeffersonn <jeffersonnlucas@gmail.com>
2017-07-04 04:52:41 -0300

    Criacao de chave para armazenar apiKey

---
+++ Wappa.Api.Driver/Settings/DriverSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wappa.Api.Driver.Settings
+{
+    public class DriverSettings
+    {
+        public string ApiKey { get; set; }
+    }
+}
