dbc81ae0a9edbc7ed9734b6565c6b3b136c1aeb3
Rodrigo Alencar <rodrigo.alencar.sm@gmail.com>
2018-06-09 22:39:10 -0300

    Create DriversRepository and it's interface to be able to unit test

---
+++ Wappa.Api.DataLayer/Repositories/DriversRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Wappa.Api.DomainModel;
+
+namespace Wappa.Api.DataLayer.Repositories
+{
+    public class DriversRepository : IDriversRepository
+	{
+		private BackOfficeContext context;
+
+		public DriversRepository(BackOfficeContext context)
+		{
+			this.context = context;
+		}
+
+		public void Add(Driver driver)
+		{
+			this.context.Drivers.Add(driver);
+		}
+    }
+}
---
+++ Wappa.Api.DataLayer/Repositories/IDriversRepository.cs
@@ -0,0 +1,9 @@
+﻿using Wappa.Api.DomainModel;
+
+namespace Wappa.Api.DataLayer.Repositories
+{
+	public interface IDriversRepository
+	{
+		void Add(Driver driver);
+	}
+}
