6f88c3416172763d95b13ff677174d730f066f20
Rodrigo Alencar <rodrigo.alencar.sm@gmail.com>
2018-06-10 19:16:39 -0300

    Update DriverController tests
    Update tests to use IGoogleGeocoderWrapper
    Add new tests to with calling to IGoogleGeocoderWrapper to verify POST behavior

--- Wappa.Api.Tests/ControllerTests/DriversControllerTests.cs
+++ Wappa.Api.Tests/ControllerTests/DriversControllerTests.cs
@@ -9,38 +9,64 @@ using Xunit;
 using Microsoft.AspNetCore.Http;
 using NSubstitute;
 using Wappa.Api.DataLayer;
+using Microsoft.AspNetCore.Mvc.Core;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using Wappa.Api.ExternalServices;
+using Wappa.Api.Responses;
 
 namespace Wappa.Api.Tests.ControllerTests
 {
 	public class DriversControllerTests
 	{
+		private static IGoogleGeocoderWrapper googleGeocoderWrapper;
 		private static IUnitOfWork unitOfWork;
+
 		private static DriversController controller;
 		private static Fixture fixture;
 
 		static DriversControllerTests()
 		{
+			googleGeocoderWrapper = Substitute.For<IGoogleGeocoderWrapper>();
 			unitOfWork = Substitute.For<IUnitOfWork>();
+
 			AutoMapper.Mapper.Initialize(cfg => cfg.AddProfile<MappingProfile>());
 
-			controller = new DriversController(unitOfWork);
+
+			controller = new DriversController(googleGeocoderWrapper, unitOfWork);
 			fixture = new Fixture();
+			RemoveThrowingBehaviorFromFixture();
+		}
+
+		private static void RemoveThrowingBehaviorFromFixture()
+		{
+			fixture.Behaviors.OfType<ThrowingRecursionBehavior>().ToList()
+							.ForEach(b => fixture.Behaviors.Remove(b));
+			fixture.Behaviors.Add(new OmitOnRecursionBehavior());
 		}
 
 		[Fact]
 		public async Task When_POST_a_Driver_should_return_Created_Driver_response_Async()
 		{
 			//Arrange
+			MockGoogleGeocoderGetAddressReturn();
+
 			var request = fixture.Create<CreateDriverRequest>();
 
 			//Act
 			var response = await controller.Post(request);
-			var result = response.Result as CreatedAtRouteResult;
+			var result = response.Result as CreatedResult;
 
 			//Assert
 			Assert.Equal(result.StatusCode, StatusCodes.Status201Created);
-			Assert.IsType<Driver>(result.Value);
+			Assert.IsType<CreatedDriverResponse>(result.Value);
+		}
+
+		private static void MockGoogleGeocoderGetAddressReturn(int howManyAddressesToMock = 1)
+		{
+			var googleAddresses = fixture.CreateMany<GoogleAddress>(howManyAddressesToMock);
+			googleGeocoderWrapper.GetAddress(Arg.Any<String>()).Returns(googleAddresses.ToList());
 		}
 
 		[Fact]
@@ -73,6 +99,8 @@ namespace Wappa.Api.Tests.ControllerTests
 		public async Task After_POST_a_Driver_should_have_call_SaveChanges_on_UnitOfWork()
 		{
 			//Arrange
+			MockGoogleGeocoderGetAddressReturn();
+
 			var request = fixture.Create<CreateDriverRequest>();
 
 			//Act
@@ -81,5 +109,38 @@ namespace Wappa.Api.Tests.ControllerTests
 			//Assert
 			await unitOfWork.Received().SaveChanges();
 		}
+
+		[Fact]
+		public async Task When_POST_a_Driver_must_call_GoogleGeocodeWrapper()
+		{
+			//Arrange
+			MockGoogleGeocoderGetAddressReturn();
+
+			var request = fixture.Create<CreateDriverRequest>();
+
+			//Act
+			var response = await controller.Post(request);
+
+			//Assert
+			await googleGeocoderWrapper.Received().GetAddress(Arg.Any<String>());
+		}
+
+		[Fact]
+		public async Task When_POST_a_Driver_and_GoogleGeocodeWrapper_returns_more_than_one_address_shoul_return_Conflict_Status()
+		{
+			//Arrange
+			var numberOfAddressesToMock = 5;
+			MockGoogleGeocoderGetAddressReturn(numberOfAddressesToMock);
+
+			var request = fixture.Create<CreateDriverRequest>();
+
+			//Act
+			var response = await controller.Post(request);
+			var result = response.Result as ObjectResult;
+
+			//Assert
+			Assert.Equal(StatusCodes.Status409Conflict, result.StatusCode);
+		}
+
 	}
 }
