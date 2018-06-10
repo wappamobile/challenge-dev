fee3665ac4551c8482f9f55ae6a0eec3d5ad7bc6
Rodrigo Alencar <rodrigo.alencar.sm@gmail.com>
2018-06-10 19:18:42 -0300

    Add CreatedDriverResponse
    CreatedDriverResponse to be used as response when POST a driver

---
+++ Wappa.Api/Responses/CreatedDriverResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Wappa.Api.Models;
+
+namespace Wappa.Api.Responses
+{
+    public class CreatedDriverResponse
+    {
+		public int Id { get; set; }
+
+		public String FirstName { get; set; }
+
+		public String LastName { get; set; }
+
+		public Address Address { get; set; }
+
+		public List<Car> Cars { get; set; }
+	}
+}
