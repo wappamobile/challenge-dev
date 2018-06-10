bb8d4b2417d7eb18a09ce3d0a620bfd0c0462013
Rodrigo Alencar <rodrigo.alencar.sm@gmail.com>
2018-06-09 22:45:26 -0300

    Add CreateDriverRequest used when POST to API

---
+++ Wappa.Api/Requests/CreateDriverRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Wappa.Api.Models;
+
+namespace Wappa.Api.Requests
+{
+	public class CreateDriverRequest
+    {
+		public int Id { get; set; }
+
+		[Required]
+		public String FirstName { get; set; }
+
+		[Required]
+		public String LastName { get; set; }
+
+		[Required]
+		public Address Address { get; set; }
+
+		[Required]
+		public List<Car> Car { get; set; }
+	}
+}
