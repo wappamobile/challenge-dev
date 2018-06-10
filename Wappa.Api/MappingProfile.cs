5be4b6704b6b53a7b803e67df045adb159000116
Rodrigo Alencar <rodrigo.alencar.sm@gmail.com>
2018-06-09 22:44:01 -0300

    Connect API to development database also add AutoMapper denpendency and a class to initialize mappings

---
+++ Wappa.Api/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Wappa.Api.DomainModel;
+using Wappa.Api.Requests;
+
+namespace Wappa.Api
+{
+    public class MappingProfile : Profile
+    {
+		public MappingProfile()
+		{
+			this.CreateMap<CreateDriverRequest, Driver>().ReverseMap();
+			this.CreateMap<Models.Driver, Driver>().ReverseMap();
+			this.CreateMap<Models.Address, Address>().ReverseMap();
+			this.CreateMap<Models.Car, Car>().ReverseMap();
+		}
+    }
+}
--- Wappa.Api/Startup.cs
+++ Wappa.Api/Startup.cs
@@ -2,46 +2,55 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Wappa.Api.DataLayer;
+using Wappa.Api.DomainModel;
+using Wappa.Api.Requests;
 
 namespace Wappa.Api
 {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 
-        public IConfiguration Configuration { get; }
+		public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-        }
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			var databaseConnection = this.Configuration.GetConnectionString("development");
+			services.AddDbContext<BackOfficeContext>(options => options.UseSqlServer(databaseConnection));
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseHsts();
-            }
+			services.AddAutoMapper(cfg => cfg.AddProfile<MappingProfile>());
+			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+		}
 
-            app.UseHttpsRedirection();
-            app.UseMvc();
-        }
-    }
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+			else
+			{
+				app.UseHsts();
+			}
+
+			app.UseHttpsRedirection();
+			app.UseMvc();
+		}
+	}
 }
