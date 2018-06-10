9db5a8074348670fd9c6d09b52a3ca6bbc978d36
Rodrigo Alencar <rodrigo.alencar.sm@gmail.com>
2018-06-09 22:46:34 -0300

    Create method POST on DriversController

---
+++ Wappa.Api/Controllers/DriversController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Wappa.Api.DataLayer;
+using Wappa.Api.DomainModel;
+using Wappa.Api.Requests;
+
+namespace Wappa.Api.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class DriversController : ControllerBase
+	{
+		private IUnitOfWork unitOfWork;
+
+		public DriversController(IUnitOfWork unitOfWork)
+		{
+			this.unitOfWork = unitOfWork;
+		}
+	
+		[HttpPost]
+		public async Task<ActionResult<Driver>> Post([FromBody] CreateDriverRequest request)
+		{
+			if (request == null) { return this.BadRequest(); }
+
+			try
+			{
+				var driver = Mapper.Map<Driver>(request);
+				this.unitOfWork.DriversRepository.Add(driver);
+				await this.unitOfWork.SaveChanges();
+				return this.CreatedAtRoute(nameof(Post), driver);
+			}
+			catch (Exception ex)
+			{
+				return this.StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+			}
+		}
+
+	}
+}
