6396322041ed989f9b220a97348d2711dcb4868e
Rodrigo Alencar <rodrigo.alencar.sm@gmail.com>
2018-06-10 19:19:15 -0300

    Update mappings
    Update AutoMapper settings

--- Wappa.Api/MappingProfile.cs
+++ Wappa.Api/MappingProfile.cs
@@ -4,7 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Wappa.Api.DomainModel;
+using Wappa.Api.ExternalServices;
 using Wappa.Api.Requests;
+using Wappa.Api.Responses;
 
 namespace Wappa.Api
 {
@@ -12,10 +14,22 @@ namespace Wappa.Api
     {
 		public MappingProfile()
 		{
-			this.CreateMap<CreateDriverRequest, Driver>().ReverseMap();
-			this.CreateMap<Models.Driver, Driver>().ReverseMap();
-			this.CreateMap<Models.Address, Address>().ReverseMap();
-			this.CreateMap<Models.Car, Car>().ReverseMap();
+			this.CreateMap<CreateDriverRequest, DomainModel.Driver>().ReverseMap();
+			this.CreateMap<CreateDriverRequest, DomainModel.Driver>().ForMember(driver => driver.Address, src => src.Ignore());
+
+			this.CreateMap<CreatedDriverResponse, DomainModel.Driver>().ReverseMap();
+
+			this.CreateMap<Models.Driver, DomainModel.Driver>().ReverseMap();
+			this.CreateMap<Models.Address, DomainModel.Address>().ReverseMap();
+			this.CreateMap<Models.Car, DomainModel.Car>().ReverseMap();
+
+			//this.CreateMap<GoogleAddress, DomainModel.Address>().ReverseMap();
+			this.CreateMap<GoogleAddress, DomainModel.Address>()
+				.ForMember(address => address.City, src => src.Ignore())
+				.ForMember(address => address.AddressLine, src => src.MapFrom(x => x.FormattedAddress))
+				.ForMember(address => address.Latitude,	src => src.MapFrom(x => x.Location.Latitude))
+				.ForMember(address => address.Longitude, src => src.MapFrom(x => x.Location.Longitude)).ReverseMap();
+	
 		}
     }
 }
