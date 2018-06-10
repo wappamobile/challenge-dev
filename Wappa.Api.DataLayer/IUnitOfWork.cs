3bb1ebdadaff207b82efe27b280edac9b1a62f35
Rodrigo Alencar <rodrigo.alencar.sm@gmail.com>
2018-06-09 22:40:24 -0300

    Create UnitOfWork with Drivers repository and it's interface

---
+++ Wappa.Api.DataLayer/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Wappa.Api.DataLayer.Repositories;
+
+namespace Wappa.Api.DataLayer
+{
+	public interface IUnitOfWork
+	{
+		IDriversRepository DriversRepository { get; }
+
+		Task<int> SaveChanges();
+	}
+}
---
+++ Wappa.Api.DataLayer/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Wappa.Api.DataLayer.Repositories;
+
+namespace Wappa.Api.DataLayer
+{
+    public class UnitOfWork : IUnitOfWork
+	{
+		private BackOfficeContext context;
+
+		public UnitOfWork(BackOfficeContext context)
+		{
+			this.context = context;
+		}
+
+		private IDriversRepository driversRepository;
+		public IDriversRepository DriversRepository
+		{
+			get
+			{
+				if (this.driversRepository == null) { this.driversRepository = new DriversRepository(this.context); }
+				return this.driversRepository;
+			}
+		}
+
+		public async Task<int> SaveChanges() => await this.context.SaveChangesAsync();
+	}
+}
