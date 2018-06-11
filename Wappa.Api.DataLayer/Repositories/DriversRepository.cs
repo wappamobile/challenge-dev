d0b30c6803f254e8bc2f7c492ecd3f1408ddf10f
Rodrigo Alencar <rodrigo.alencar.sm@gmail.com>
2018-06-10 22:55:08 -0300

    Update Driver repository with a GetAll method with filter parameters

--- Wappa.Api.DataLayer/Repositories/DriversRepository.cs
+++ Wappa.Api.DataLayer/Repositories/DriversRepository.cs
@@ -1,12 +1,17 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using Wappa.Api.DomainModel;
 
 namespace Wappa.Api.DataLayer.Repositories
 {
-    public class DriversRepository : IDriversRepository
+	public class DriversRepository : IDriversRepository
 	{
+		private static readonly String LAST_NAME_SORT_FILTER = "lastname" ;
+
 		private BackOfficeContext context;
 
 		public DriversRepository(BackOfficeContext context)
@@ -18,5 +23,40 @@ namespace Wappa.Api.DataLayer.Repositories
 		{
 			this.context.Drivers.Add(driver);
 		}
-    }
+
+		public async Task<IList<Driver>> GetAll(String sortBy, int limit, int offset)
+		{
+			var sortedDrivers = default(IQueryable<Driver>);
+			var drivers = default(IList<Driver>);
+
+			if (sortBy == LAST_NAME_SORT_FILTER)
+			{
+				sortedDrivers = this.SortDriversByLastName();
+				drivers = this.ApplyLimitAndOffset(sortedDrivers, limit, offset);
+				return await Task.FromResult(drivers);
+			}
+
+			sortedDrivers = this.SortDriversByFirstName();
+			drivers = this.ApplyLimitAndOffset(sortedDrivers, limit, offset);
+
+			return await Task.FromResult(drivers);
+		}
+
+		private IQueryable<Driver> SortDriversByLastName()
+		{
+			return this.context.Drivers.Include(a => a.Address).Include(c => c.Cars)
+				.OrderBy(d => d.LastName);
+		}
+
+		private IList<Driver> ApplyLimitAndOffset(IQueryable<Driver> drivers, int limit, int offset)
+		{
+			return drivers.Skip(offset).Take(limit).ToList();
+		}
+
+		private IQueryable<Driver> SortDriversByFirstName()
+		{
+			return this.context.Drivers.Include(a => a.Address).Include(c => c.Cars)
+				.OrderBy(d => d.FirstName);
+		}
+	}
 }
--- Wappa.Api.DataLayer/Repositories/IDriversRepository.cs
+++ Wappa.Api.DataLayer/Repositories/IDriversRepository.cs
@@ -1,9 +1,14 @@
-﻿using Wappa.Api.DomainModel;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Wappa.Api.DomainModel;
 
 namespace Wappa.Api.DataLayer.Repositories
 {
 	public interface IDriversRepository
 	{
 		void Add(Driver driver);
+
+		Task<IList<Driver>> GetAll(String sortBy, int limit, int offset);
 	}
 }
