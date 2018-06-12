1bb4e0f7ef01c5ef2d3a98f8be1aa8b475814769
Rodrigo Alencar <rodrigo.alencar.sm@gmail.com>
2018-06-11 21:47:52 -0300

    Move HasMoreThanOnePossibleAddress method upper on class

--- Wappa.Api/Controllers/DriversController.cs
+++ Wappa.Api/Controllers/DriversController.cs
@@ -116,6 +116,11 @@ namespace Wappa.Api.Controllers
 			}
 		}
 
+		private bool HasMoreThanOnePossibleAddress(IList<GoogleAddress> possibleAddressesOnGoogle)
+		{
+			return possibleAddressesOnGoogle.Count > 1;
+		}
+
 		private Driver CreateDriverFromRequestAndGoogleAddress(CreateDriverRequest request, GoogleAddress driverAddressOnGoogle)
 		{
 			var driver = Mapper.Map<Driver>(request);
