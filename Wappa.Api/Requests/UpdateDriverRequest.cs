77d2e13136ce359ddcefe2edeb64de987615ff84
Rodrigo Alencar <rodrigo.alencar.sm@gmail.com>
2018-06-11 23:06:20 -0300

    Create UpdateDriverRequest

---
+++ Wappa.Api/Requests/UpdateDriverRequest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Wappa.Api.Models;
+
+namespace Wappa.Api.Requests
+{
+    public class UpdateDriverRequest
+    {
+		[Required]
+		public int Id { get; set; }
+
+		[Required]
+		public String FirstName { get; set; }
+
+		[Required]
+		public String LastName { get; set; }
+
+		[Required]
+		public Address Address { get; set; }
+
+		[Required]
+		public List<Car> Cars { get; set; }
+	}
+}
