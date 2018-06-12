d39e316aa75b2a25a2ec95f9b384d29ae854132c
Rodrigo Alencar <rodrigo.alencar.sm@gmail.com>
2018-06-11 23:47:06 -0300

    Create address repository and its interface with update method

---
+++ Wappa.Api.DataLayer/Repositories/AddressRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wappa.Api.DomainModel;
+
+namespace Wappa.Api.DataLayer.Repositories
+{
+    public class AddressRepository : IAddressRepository
+    {
+		private BackOfficeContext context;
+
+		public AddressRepository(BackOfficeContext context)
+		{
+			this.context = context;
+		}
+
+		public async Task Update(Address address)
+		{
+			await Task.FromResult(this.context.Addresses.Update(address));
+		}
+	}
+}
---
+++ Wappa.Api.DataLayer/Repositories/IAddressRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Wappa.Api.DomainModel;
+
+namespace Wappa.Api.DataLayer.Repositories
+{
+	public interface IAddressRepository
+	{
+		Task Update(Address address);
+	}
+}
