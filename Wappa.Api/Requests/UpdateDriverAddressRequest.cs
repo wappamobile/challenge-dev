1f2dfd8fc22ecd1357f620cfab948af2a803bf8a
Rodrigo Alencar <rodrigo.alencar.sm@gmail.com>
2018-06-11 23:45:41 -0300

    Create UpdateDriverAddressRequest

---
+++ Wappa.Api/Requests/UpdateDriverAddressRequest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wappa.Api.Requests
+{
+    public class UpdateDriverAddressRequest
+    {
+		[Required]
+		public int Id { get; set; }
+
+		[Required]
+		public String AddressLine { get; set; }
+
+		[Required]
+		public String PostalCode { get; set; }
+
+		[Required]
+		public String City { get; set; }
+
+		[Required]
+		public String State { get; set; }
+	}
+}
