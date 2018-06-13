8c667226ab10c76b4fed4de5814eafde25bed1ce
Rodrigo Alencar <rodrigo.alencar.sm@gmail.com>
2018-06-12 21:16:42 -0300

    Add DatabaseBootstrap sql file

---
+++ Wappa.Api.DataLayer/SQL/DatabaseBootstrap.sql
@@ -0,0 +1,78 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180612212614_DatabaseBootstrap')
+BEGIN
+    CREATE TABLE [Drivers] (
+        [Id] int NOT NULL IDENTITY,
+        [FirstName] nvarchar(max) NOT NULL,
+        [LastName] nvarchar(max) NOT NULL,
+        CONSTRAINT [PK_Drivers] PRIMARY KEY ([Id])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180612212614_DatabaseBootstrap')
+BEGIN
+    CREATE TABLE [Addresses] (
+        [Id] int NOT NULL IDENTITY,
+        [DriverId] int NOT NULL,
+        [AddressLine] nvarchar(max) NOT NULL,
+        [PostalCode] nvarchar(max) NOT NULL,
+        [City] nvarchar(max) NOT NULL,
+        [Latitude] nvarchar(max) NOT NULL,
+        [Longitude] nvarchar(max) NOT NULL,
+        [State] nvarchar(max) NOT NULL,
+        CONSTRAINT [PK_Addresses] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_Addresses_Drivers_DriverId] FOREIGN KEY ([DriverId]) REFERENCES [Drivers] ([Id]) ON DELETE CASCADE
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180612212614_DatabaseBootstrap')
+BEGIN
+    CREATE TABLE [Cars] (
+        [Id] int NOT NULL IDENTITY,
+        [DriverId] int NOT NULL,
+        [LicensePlate] nvarchar(max) NULL,
+        [Model] nvarchar(max) NULL,
+        [Vendor] nvarchar(max) NULL,
+        CONSTRAINT [PK_Cars] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_Cars_Drivers_DriverId] FOREIGN KEY ([DriverId]) REFERENCES [Drivers] ([Id]) ON DELETE CASCADE
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180612212614_DatabaseBootstrap')
+BEGIN
+    CREATE UNIQUE INDEX [IX_Addresses_DriverId] ON [Addresses] ([DriverId]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180612212614_DatabaseBootstrap')
+BEGIN
+    CREATE INDEX [IX_Cars_DriverId] ON [Cars] ([DriverId]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180612212614_DatabaseBootstrap')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20180612212614_DatabaseBootstrap', N'2.1.0-rtm-30799');
+END;
+
+GO
+
