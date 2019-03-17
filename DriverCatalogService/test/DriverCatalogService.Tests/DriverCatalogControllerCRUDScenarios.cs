aee10c73754bee4943a182914975583aeeec6361
hbulhoes <humberto.bulhoes@gmail.com>
2019-03-17 14:15:01 -0300

    Commit inicial

---
+++ DriverCatalogService/test/DriverCatalogService.Tests/DriverCatalogControllerCRUDScenarios.cs
@@ -0,0 +1,51 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace DriverCatalogService.Tests
+{
+    public class DriverCatalogControllerCRUDScenarios
+    {
+        [Fact]
+        public async Task Creating_a_driver_with_valid_data()
+        {
+
+        }
+        
+        [Fact]
+        public async Task Trying_to_create_a_driver_with_invalid_data()
+        {
+
+        }
+
+        [Fact]
+        public async Task Updating_a_driver_with_valid_data()
+        {
+
+        }
+
+        [Fact]
+        public async Task Trying_to_update_of_a_driver_with_invalid_data()
+        {
+
+        }
+
+        [Fact]
+        public async Task Trying_to_update_a_driver_that_does_not_exist()
+        {
+
+        }
+
+        [Fact]
+        public async Task Deleting_a_driver()
+        {
+
+        }
+
+        [Fact]
+        public async Task Trying_to_delete_a_driver_that_does_not_exist()
+        {
+
+        }
+
+    }
+}
---
+++ DriverCatalogService/test/DriverCatalogService.Tests/DriverCatalogQueryScenarios.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace DriverCatalogService.Tests
+{
+    public class DriverCatalogQueryScenarios
+    {
+        [Fact]
+        public async Task Listing_all_drivers_sorting_by_first_name()
+        {
+
+        }
+
+        [Fact]
+        public async Task Listing_all_drivers_sorting_by_last_name()
+        {
+
+        }
+    }
+}
