06ee89c9fa18e170ff8ceb798f967c59a8d00922
Phillipe Roger Souza <phillrog@hotmail.com>
2021-03-09 03:57:02 -0300

    feature/TAR-1-Criar-API-Motorista #comment - Foi ignorado propriedade validationresult

--- src/building blocks/Wappa.Core/Messages/Command.cs
+++ src/building blocks/Wappa.Core/Messages/Command.cs
@@ -1,12 +1,14 @@
 ﻿using FluentValidation.Results;
 using MediatR;
 using System;
+using System.Text.Json.Serialization;
 
 namespace Wappa.Core.Messages
 {
 	public abstract class Command : Message, IRequest<ValidationResult>
 	{
 		public DateTime TimeStamp { get; private set; }
+		[JsonIgnore]
 		public ValidationResult ValidationResult { get; set; }
 
 		protected Command()
