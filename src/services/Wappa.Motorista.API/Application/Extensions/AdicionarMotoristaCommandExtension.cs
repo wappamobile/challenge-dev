ae556a7076376fa3cd75b202b25a042d1ff60943
Phillipe Roger Souza <phillrog@hotmail.com>
2021-03-09 03:41:06 -0300

    feature/TAR-1-Criar-API-Motorista #comment - Foi criado o méotodo para mapear o comando adicionar

---
+++ src/services/Wappa.Motorista.API/Application/Extensions/AdicionarMotoristaCommandExtension.cs
@@ -0,0 +1,31 @@
+﻿using Wappa.Motoristas.API.Application.Commands;
+using Wappa.Motoristas.API.Models;
+
+namespace Wappa.Motoristas.API.Application.Extensions
+{
+	public static class AdicionarMotoristaCommandExtension
+    {
+        public static Motorista MapearMotorista(this AdicionarMotoristaCommand message)
+        {
+            var endereco = new Endereco
+            {
+                Logradouro = message.Endereco.Logradouro,
+                Numero = message.Endereco.Numero,
+                Complemento = message.Endereco.Complemento,
+                Bairro = message.Endereco.Bairro,
+                Cep = message.Endereco.Cep,
+                Cidade = message.Endereco.Cidade,
+                Estado = message.Endereco.Estado
+            };
+
+            var carro = new Carro(message.Carro.Marca, message.Carro.Modelo, message.Carro.Placa);
+
+            var motorista = new Motorista(message.Nome, message.SobreNome);
+
+            motorista.Carro = carro;
+            motorista.Endereco = endereco;
+
+            return motorista;
+        }
+    }
+}
