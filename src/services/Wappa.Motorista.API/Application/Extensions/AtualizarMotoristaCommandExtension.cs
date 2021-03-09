9d3062156c3957ed9531c3479b7db7d9260b445b
Phillipe Roger Souza <phillrog@hotmail.com>
2021-03-09 03:41:51 -0300

    feature/TAR-1-Criar-API-Motorista #comment - Foi criado a classe estática para usar o método de extensão que mapea o comando atualizar

---
+++ src/services/Wappa.Motorista.API/Application/Extensions/AtualizarMotoristaCommandExtension.cs
@@ -0,0 +1,28 @@
+﻿using Wappa.Motoristas.API.Application.Commands;
+using Wappa.Motoristas.API.Models;
+
+namespace Wappa.Motoristas.API.Application.Extensions
+{
+	public static class AtualizarMotoristaCommandExtension
+    {
+        public static Motorista MapearMotorista(this AtualizarMotoristaCommand message, Motorista motorista)
+        {
+            motorista.Carro.Marca = message.Carro.Marca;
+            motorista.Carro.Modelo = message.Carro.Modelo;
+            motorista.Carro.Placa = message.Carro.Placa;
+
+            motorista.Endereco.Logradouro = message.Endereco.Logradouro;
+            motorista.Endereco.Numero = message.Endereco.Numero;
+            motorista.Endereco.Complemento = message.Endereco.Complemento;
+            motorista.Endereco.Bairro = message.Endereco.Bairro;
+            motorista.Endereco.Cep = message.Endereco.Cep;
+            motorista.Endereco.Cidade = message.Endereco.Cidade;
+            motorista.Endereco.Estado = message.Endereco.Estado;
+
+            motorista.Nome = message.Nome;
+            motorista.SobreNome = message.SobreNome;
+
+            return motorista;
+        }
+    }
+}
