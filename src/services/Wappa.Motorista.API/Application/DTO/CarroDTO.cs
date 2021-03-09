211f2fbbdbf6bc0d6a728fa65c5d016ed47e1420
Phillipe Roger Souza <phillrog@hotmail.com>
2021-03-09 02:30:08 -0300

    feature/TAR-1-Criar-API-Motorista #comment - Foi criado o DTO para montar o motorista

---
+++ src/services/Wappa.Motorista.API/Application/DTO/CarroDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Wappa.Motoristas.API.Application.DTO
+{
+    public class CarroDTO
+    {
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Placa { get; set; }        
+    }
+}
