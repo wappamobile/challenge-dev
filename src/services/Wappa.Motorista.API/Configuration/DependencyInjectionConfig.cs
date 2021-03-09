5e004d6d66171a9684ed99583d3b0785c65fd64e
Phillipe Roger Souza <phillrog@hotmail.com>
2021-03-09 02:31:59 -0300

    feature/TAR-1-Criar-API-Motorista #comment - Foi configurado a DI

--- src/services/Wappa.Motorista.API/Configuration/DependencyInjectionConfig.cs
+++ src/services/Wappa.Motorista.API/Configuration/DependencyInjectionConfig.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using FluentValidation.Results;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Wappa.Core.Mediator;
+using Wappa.Motoristas.API.Application.Commands;
 using Wappa.Motoristas.API.Data;
 using Wappa.Motoristas.API.Data.Repository;
 using Wappa.Motoristas.API.Models;
@@ -8,7 +12,14 @@ namespace Wappa.Motoristas.API.Configuration
 	public static class DependencyInjectionConfig
     {
         public static void RegisterServices(this IServiceCollection services)
-        {            
+        {
+            // Commands
+            services.AddScoped<IRequestHandler<AdicionarMotoristaCommand, ValidationResult>, MotoristaCommandHandler>();
+
+            // Application
+            services.AddScoped<IMediatorHandler, MediatorHandler>();
+
+            // Data
             services.AddScoped<IMotoristaRepository, MotoristaRepository>();
             services.AddScoped<MotoristaContext>();
         }
