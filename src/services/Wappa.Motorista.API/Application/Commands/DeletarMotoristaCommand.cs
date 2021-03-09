fe4a493acda4d97bf7aa8bb3bc1885c0afdab594
Phillipe Roger Souza <phillrog@hotmail.com>
2021-03-09 03:55:50 -0300

    feature/TAR-1-Criar-API-Motorista #comment - Foi criado o comando deletar

---
+++ src/services/Wappa.Motorista.API/Application/Commands/DeletarMotoristaCommand.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using Wappa.Core.Messages;
+using System;
+
+namespace Wappa.Motoristas.API.Application.Commands
+{
+	public class DeletarMotoristaCommand : Command
+    {
+		// Motorista
+		public Guid Id { get; set; }
+        public override bool EhValido()
+        {
+            ValidationResult = new DeletarMotoristaValidation().Validate(this);
+            return ValidationResult.IsValid;
+        }        
+
+        public class DeletarMotoristaValidation : AbstractValidator<DeletarMotoristaCommand>
+        {
+            public DeletarMotoristaValidation()
+            {
+                RuleFor(c => c.Id)
+                    .NotEqual(Guid.Empty)
+                    .WithMessage("Id do motorista inválido");
+            }
+        }
+    }
+}
