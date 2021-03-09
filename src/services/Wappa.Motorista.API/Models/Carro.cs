b43cb6a3b59733c061e11d4f7685c6424cbcf03c
Phillipe Roger Souza <phillrog@hotmail.com>
2021-03-08 22:09:40 -0300

    feature/TAR-1-Criar-API-Motorista #comment - FOi criado as entidades

---
+++ src/services/Wappa.Motorista.API/Models/Carro.cs
@@ -0,0 +1,24 @@
+﻿using NSE.Core.DomainObjects;
+using System;
+
+namespace Wappa.Motorista.API.Models
+{
+	public class Carro: Entity
+	{
+        public string Marca { get; private set; }
+        public string Modelo { get; private set; }
+        public string Placa { get; private set; }
+        
+        public Guid MotoristaId { get; private set; }
+
+        // EF Relation
+        public Motorista Motorista { get; protected set; }
+
+        public Carro(string marca, string modelo, string placa)
+        {
+            Marca = marca;
+            Modelo = modelo;
+            Placa = placa;            
+        }
+    }
+}
---
+++ src/services/Wappa.Motorista.API/Models/Motorista.cs
@@ -0,0 +1,22 @@
+﻿using NSE.Core.DomainObjects;
+using System;
+
+namespace Wappa.Motorista.API.Models
+{
+	public class Motorista: Entity, IAggregateRoot
+    {
+        public string Nome { get; private set; }
+        public string SobreNome { get; private set; }
+        public Carro Carro { get; private set; }
+        public Endereco Endereco { get; private set; }
+
+        protected Motorista() { }
+
+        public Motorista(Guid id, string nome, string sobreNome)
+        {
+            Id = id;
+            Nome = nome;
+            SobreNome = sobreNome;
+        }
+    }
+}
