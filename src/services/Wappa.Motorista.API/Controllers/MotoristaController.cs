75e521bbec85ae033dc6740d84c50795e30e5cbd
Phillipe Roger Souza <phillrog@hotmail.com>
2021-03-09 02:33:01 -0300

    feature/TAR-1-Criar-API-Motorista #comment - Foi criado o primeiro método POST para criar um motorista

--- src/services/Wappa.Motorista.API/Controllers/MotoristaController.cs
+++ src/services/Wappa.Motorista.API/Controllers/MotoristaController.cs
@@ -4,13 +4,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Wappa.Core.Mediator;
+using Wappa.Motoristas.API.Application.Commands;
 
 namespace Wappa.Motoristas.API.Controllers
 {
-	[ApiController]
-	[Route("[controller]")]
-	public class MotoristaController : ControllerBase
+	[Route("api/motorista")]
+	public class MotoristaController : MainController
 	{
-		
+		private readonly IMediatorHandler _mediator;
+
+		public MotoristaController(IMediatorHandler mediator)
+		{
+			_mediator = mediator;
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> AdicionarMotorista(AdicionarMotoristaCommand motorista)
+		{
+			return CustomResponse(await _mediator.EnviarComando(motorista));
+		}
 	}
 }
