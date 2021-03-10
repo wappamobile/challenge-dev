9a002443c9e8781ceea28952615bb6730a7a1834
Phillipe Roger Souza <phillrog@hotmail.com>
2021-03-09 21:37:02 -0300

    feature/TAR-1-Criar-API-Motorista #comment - Foi colocado alguns comentários para documentar a api

--- src/services/Wappa.Motorista.API/Controllers/MotoristaController.cs
+++ src/services/Wappa.Motorista.API/Controllers/MotoristaController.cs
@@ -1,15 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using NSE.Pedidos.API.Application.Queries;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Wappa.Core.Mediator;
 using Wappa.Motoristas.API.Application.Commands;
+using System.Net;
+using FluentValidation.Results;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace Wappa.Motoristas.API.Controllers
 {
+	/// <summary>
+	/// cadastro de motoristas
+	/// </summary>
 	[Route("api/motorista")]
 	public class MotoristaController : MainController
 	{
@@ -23,25 +25,53 @@ namespace Wappa.Motoristas.API.Controllers
 			_motoristaQueries = motoristaQueries;
 		}
 
+		/// <summary>
+		/// Adiciona um novo motorista ao sistema
+		/// </summary>
+		/// <param name="motorista">Dados do motoristas</param>
+		/// <returns>Retorna um 200 ou 400</returns>
 		[HttpPost]
+		[SwaggerResponse((int)HttpStatusCode.OK, Description = "Retorna quando sucesso.")]
+		[SwaggerResponse((int)HttpStatusCode.BadRequest, Description = "Retorna quando ocorre alguma falha.")]
 		public async Task<IActionResult> AdicionarMotorista(AdicionarMotoristaCommand motorista)
 		{
 			return CustomResponse(await _mediator.EnviarComando(motorista));
 		}
 
+		/// <summary>
+		/// Atualiza os dados de um motorista cadastrado no sistema
+		/// </summary>
+		/// <param name="motorista">Dados do motoristas</param>
+		/// <returns>Retorna um 200 ou 400</returns>
 		[HttpPut]
+		[SwaggerResponse((int)HttpStatusCode.OK, Description = "Retorna quando sucesso.")]
+		[SwaggerResponse((int)HttpStatusCode.BadRequest, Description = "Retorna quando ocorre alguma falha.")]
 		public async Task<IActionResult> AtualizarMotorista(AtualizarMotoristaCommand motorista)
 		{
 			return CustomResponse(await _mediator.EnviarComando(motorista));
 		}
 
+		/// <summary>
+		/// Deleta o cadastro do motorista do sistema
+		/// </summary>
+		/// <param name="motorista">Dados do motoristas</param>
+		/// <returns>Retorna um 200 ou 400</returns>
 		[HttpDelete]
+		[SwaggerResponse((int)HttpStatusCode.OK, Description = "Retorna quando sucesso.", Type = typeof(ValidationResult))]
+		[SwaggerResponse((int)HttpStatusCode.BadRequest, Description = "Retorna quando ocorre alguma falha.", Type = typeof(ValidationResult))]
 		public async Task<IActionResult> DeletarMotorista(DeletarMotoristaCommand motorista)
 		{
 			return CustomResponse(await _mediator.EnviarComando(motorista));
 		}
 
+		/// <summary>
+		/// Busca todos os motoristas cadastrados no sistema
+		/// </summary>
+		/// <returns>Retorna um 200 ou 400 ou 404</returns>
 		[HttpGet]
+		[SwaggerResponse((int)HttpStatusCode.OK, Description = "Retorna quando sucesso.")]
+		[SwaggerResponse((int)HttpStatusCode.BadRequest, Description = "Retorna quando ocorre alguma falha.")]
+		[SwaggerResponse((int)HttpStatusCode.NotFound, Description = "Retorna quando ocorre alguma falha.")]
 		public async Task<IActionResult> ListarMotorista()
 		{
 			var motoristas = await _motoristaQueries.ObterListaMotoristas();
