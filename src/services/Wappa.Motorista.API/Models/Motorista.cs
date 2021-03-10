11a03edd2f292bc6610094058fa4ca1d79fe3a60
Phillipe Roger Souza <phillrog@hotmail.com>
2021-03-10 02:40:20 -0300

    feature/TAR-1-Criar-API-Motorista #comment - Limpeza

--- src/services/Wappa.Motorista.API/Models/Motorista.cs
+++ src/services/Wappa.Motorista.API/Models/Motorista.cs
@@ -1,21 +1,22 @@
 ﻿using Wappa.Core.DomainObjects;
 using System;
+using FluentValidation;
 
 namespace Wappa.Motoristas.API.Models
 {
-	public class Motorista: Entity, IAggregateRoot
-    {
-        public string Nome { get; set; }
-        public string SobreNome { get;  set; }
-        public Carro Carro { get; set; }
-        public Endereco Endereco { get; set; }
+	public class Motorista : Entity, IAggregateRoot
+	{
+		public string Nome { get; set; }
+		public string SobreNome { get; set; }
+		public Carro Carro { get; set; }
+		public Endereco Endereco { get; set; }
 
-        protected Motorista() { }
+		protected Motorista() { }
 
-        public Motorista(string nome, string sobreNome)
-        {
-            Nome = nome;
-            SobreNome = sobreNome;
-        }
-    }
+		public Motorista(string nome, string sobreNome)
+		{
+			Nome = nome;
+			SobreNome = sobreNome;
+		}		
+	}
 }
