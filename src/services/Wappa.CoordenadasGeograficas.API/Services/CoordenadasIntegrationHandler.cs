c972ac8063cef737324e7b34de9456689983acc7
Phillipe Roger Souza <phillrog@hotmail.com>
2021-03-10 02:38:01 -0300

    feature/TAR-1-Criar-API-Motorista #comment - Foi criado o handler para assinar o respond

---
+++ src/services/Wappa.CoordenadasGeograficas.API/Services/CoordenadasIntegrationHandler.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Wappa.Core.Messages.Integration;
+using Wappa.MessageBus;
+
+namespace Wappa.CoordenadasGeograficas.API.Services
+{
+	public class CoordenadasIntegrationHandler : BackgroundService
+	{
+		private readonly IMessageBus _bus;
+
+		public CoordenadasIntegrationHandler(IMessageBus bus, IServiceProvider serviceProvider)
+		{
+			_bus = bus;			
+		}
+
+		private void SetResponder()
+		{
+			_bus.RespondAsync<SolicitouCadastroMotoristaIntegrationEvent, ResponseMessage>(async request =>
+				await ConsultarCoordenada(request));
+		}
+
+		private Task<ResponseMessage> ConsultarCoordenada(SolicitouCadastroMotoristaIntegrationEvent request)
+		{
+			return Task.FromResult(new ResponseMessage(null));
+		}
+
+		protected override Task ExecuteAsync(CancellationToken stoppingToken)
+		{
+			SetResponder();
+			return Task.CompletedTask;
+		}
+	}
+}
