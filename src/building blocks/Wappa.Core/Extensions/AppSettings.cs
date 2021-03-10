75587b0a93d3ae1a604179c6c057690c1037b8e7
Phillipe Roger Souza <phillrog@hotmail.com>
2021-03-10 01:13:25 -0300

    feature/TAR-1-Criar-API-Motorista #comment - Foi criado um appsettings para guardar informações da consulta

---
+++ src/building blocks/Wappa.Core/Extensions/AppSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Wappa.Core.Extensions
+{
+	public class AppSettings
+    {
+        public string GoogleGeocodingUrl { get; set; }        
+        public string GoogleAPiKey { get; set; }
+    }
+}
