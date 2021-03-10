ccb7b5f4482cdfbf0e402bca51460a55e4ed95c1
Phillipe Roger Souza <phillrog@hotmail.com>
2021-03-10 01:14:38 -0300

    feature/TAR-1-Criar-API-Motorista #comment - Foi criado o método e injetado o serviço

--- src/services/Wappa.CoordenadasGeograficas.API/Controllers/CoordenadaGeograficaController.cs
+++ src/services/Wappa.CoordenadasGeograficas.API/Controllers/CoordenadaGeograficaController.cs
@@ -3,6 +3,7 @@ using Swashbuckle.AspNetCore.Annotations;
 using System.Net;
 using System.Threading.Tasks;
 using Wappa.CoordenadasGeograficas.API.Models;
+using Wappa.CoordenadasGeograficas.API.Services;
 using Wappa.WebAPI.Core.Controllers;
 
 namespace Wappa.CoordenadasGeograficas.API.Controllers
@@ -13,18 +14,25 @@ namespace Wappa.CoordenadasGeograficas.API.Controllers
 	[Route("api/geocoordenada")]
 	public class CoordenadaGeograficaController : MainController
 	{
+		private readonly IGoogleGeocondingService _googleGeocondingService;
+
+		public CoordenadaGeograficaController(IGoogleGeocondingService googleGeocondingService)
+		{
+			_googleGeocondingService = googleGeocondingService;
+		}
+
 		/// <summary>
 		/// Busca coordenada geográfica de um endereço
 		/// </summary>
 		/// <param name="endereco">Dados do endereço</param>
 		/// <returns>200 ou 400 ou 404</returns>
-		[HttpGet]
+		[HttpPost]
 		[SwaggerResponse((int)HttpStatusCode.OK, Description = "Retorna quando sucesso.")]
 		[SwaggerResponse((int)HttpStatusCode.BadRequest, Description = "Retorna quando ocorre alguma falha.")]
 		[SwaggerResponse((int)HttpStatusCode.NotFound, Description = "Retorna quando ocorre alguma falha.")]
 		public async Task<IActionResult> BuscarCoordenadaPorEndereco(Endereco endereco)
 		{
-			return Ok();
+			return CustomResponse(await _googleGeocondingService.BuscarCoordenadas(endereco));
 		}
 	}
 }
