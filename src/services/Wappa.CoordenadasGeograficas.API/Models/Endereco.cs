5b1f977d4d4711f136e55543a6960484bf696f7e
Phillipe Roger Souza <phillrog@hotmail.com>
2021-03-10 00:59:27 -0300

    feature/TAR-1-Criar-API-Motorista - Foi criado o modelo endereco

--- src/services/Wappa.CoordenadasGeograficas.API/Models/Endereco.cs
+++ src/services/Wappa.CoordenadasGeograficas.API/Models/Endereco.cs
@@ -2,14 +2,19 @@
 
 namespace Wappa.CoordenadasGeograficas.API.Models
 {
-    public class Endereco
-    {
-        public string Logradouro { get; set; }
-        public string Numero { get; set; }
-        public string Complemento { get; set; }
-        public string Bairro { get; set; }
-        public string Cep { get; set; }
-        public string Cidade { get; set; }
-        public string Estado { get; set; }
-    }
+	public class Endereco
+	{
+		public string Logradouro { get; set; }
+		public string Numero { get; set; }
+		public string Complemento { get; set; }
+		public string Bairro { get; set; }
+		public string Cep { get; set; }
+		public string Cidade { get; set; }
+		public string Estado { get; set; }
+
+		public string Consultar()
+		{
+			return string.Join(Logradouro, "+", Numero, "+", Complemento, "+", Bairro, "+", Cep, "+", Cidade, "+", Estado);
+		}
+	}
 }
