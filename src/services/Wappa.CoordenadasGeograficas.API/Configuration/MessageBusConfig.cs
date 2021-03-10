f4fb9c0a815f25c896acedb0ea36d7b4ca1d389b
Phillipe Roger Souza <phillrog@hotmail.com>
2021-03-10 02:34:55 -0300

    feature/TAR-1-Criar-API-Motorista #comment - Foi criado o config messagebus setando o handler

---
+++ src/services/Wappa.CoordenadasGeograficas.API/Configuration/MessageBusConfig.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Wappa.CoordenadasGeograficas.API.Services;
+using Wappa.Core.Utils;
+using Wappa.MessageBus;
+
+namespace Wappa.CoordenadasGeograficas.API.Configuration
+{
+    public static class MessageBusConfig
+    {
+        public static void AddMessageBusConfiguration(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddMessageBus(configuration.GetMessageQueueConnection("MessageBus"))
+                .AddHostedService<CoordenadasIntegrationHandler>();
+        }
+    }
+}
